3925203de6d684c75811e45b8935170e244c7af3
Prem S. Pandey <premspandey123#gmail.com>
2024-06-29 08:31:00 +0530

    Configured Vagrant and setup hello world script

---
+++ simple-node-project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ sudo apt-get update -y
+
+ ## Git ##
+ echo '###Installing Git..'
+ sudo apt-get install git -y
+
+ sudo apt install -y python3.11
+
+echo "python3 has been installed successfully."
